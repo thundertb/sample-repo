5e0ddf1e231b787c356630ff7587965a5dc50d49
dheerajmavilla <64136097+dheerajmavilla@users.noreply.github.com>
2022-09-17 19:09:42 +0530

    exec sc

---
+++ terraform/scripts/old.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+export OUT_FILE=terraform/unittest_output.txt
+
+start=$(grep -n "FAIL " $OUT_FILE |cut -f1 -d:|head -n 1)
+end=$(grep -n "FAIL" $OUT_FILE |cut -f1 -d:|tail -n 1)
+
+sed -n $start,"$end"p $OUT_FILE
